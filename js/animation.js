d2dfc1909c5aea177d63cd5250ef9abacc8dc3d8
stiven <brayan-steven23@hotmail.com>
2023-04-29 00:04:59 -0500

    agregue la libreria de scroll reveal, concluido el proyecto hasta ahora

---
+++ js/animation.js
@@ -0,0 +1,36 @@
+window.sr = ScrollReveal ();
+
+sr.reveal('.showcase', {
+    duration: 1500, 
+    origin: 'right',
+    distance: '-300px',
+    delay: 500
+}); 
+
+sr.reveal('.news-cards', {
+    duration: 2000, 
+    origin: 'bottom',
+    distance: '300px',
+    delay: 500
+}); 
+
+sr.reveal('.cards-banner-one', {
+    duration: 2000, 
+    origin: 'top',
+    distance: '-300px',
+    delay: 500
+}); 
+
+sr.reveal('.cards-banner-two', {
+    duration: 2000, 
+    origin: 'top',
+    distance: '-300px',
+    delay: 500
+}); 
+
+sr.reveal('.social', {
+    duration: 2000, 
+    origin: 'right',
+    distance: '-300px',
+    delay: 500
+}); 
